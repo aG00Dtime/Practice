c11789fc4536dc4bdc7f74bb6d1d047aaa28613d
aG00Dtime <77511476+aG00Dtime@users.noreply.github.com>
2021-02-05 12:01:20 -0400

    New

--- chance.py
+++ chance.py
@@ -1,6 +1,11 @@
 import random
+from collections import Counter
+
+bag = {"potion": 2, "escape-rope": 1, "power-up": 0}
+loot_pool = ["potion", "escape-rope", "power-up"]
 
 
+# function to determine spawn
 def chance(percent):
     x = random.randint(0, 100)
     if x <= percent:
@@ -9,15 +14,32 @@ def chance(percent):
         return False
 
 
-i_h = 0
-n_h = 0
-for i in range(10):
+# pick some loot
+def loot():
 
-    if chance(50):
+    loot_dropped = random.choices(loot_pool, weights=[10, 7, 5], k=2)
 
-        i_h += 1
+    for loot in loot_dropped:
 
-    else:
+        if loot == "potion":
+            bag["potion"] += 1
 
-        n_h += 1
-print(i_h, n_h)
+        elif loot == "escape-rope":
+            bag["escape-rope"] += 1
+
+        elif loot == "power-up":
+            bag["power-up"] += 1
+        else:
+            break
+
+        # count the number of items dropped
+    c = Counter(loot_dropped)
+
+    for keys, values in c.items():
+        print("You found a", keys.capitalize(), "x", values, "!")
+
+
+if chance(50):
+    loot()
+else:
+    print("Unlucky")
